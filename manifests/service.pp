fbea66e8058b7eae8abfb93c48f1b92541683953
pfhonore <pfhonore@users.noreply.github.com>
2018-04-03 18:09:21 +0200

    Update service.pp

--- manifests/service.pp
+++ manifests/service.pp
@@ -10,6 +10,7 @@ class fusioninventory::service inherits ::fusioninventory {
     group   => 'wheel',
     mode    => '0755',
     content => template('fusioninventory/agent.cfg.erb'),
+    notify  => Service['org.fusioninventory.agent'],
     }
       }
       default:             {
